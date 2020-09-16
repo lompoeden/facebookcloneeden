c202bd5905b6e31e9924dcd0cfbecc79ed0f9540
lompo <lompo@iuj.ac.jp>
2020-09-16 16:34:36 +0900

    added db

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,33 @@
 
 ActiveRecord::Schema.define(version: 2020_09_16_062229) do
 
+  enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "content"
+  end
+
   create_table "feeds", force: :cascade do |t|
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.string "title"
+    t.string "content"
+    t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index [nil], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "feeds", "users"
 end
